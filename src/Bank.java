4c56a9ad159d7d55791b54a427bfc6a4c87ebaf5
VijayakumarPeriasamy <vijayperiasamynkl@gmail.com>
2019-12-14 09:17:27 +0530

    Line sample 3 completed

--- src/Bank.java
+++ src/Bank.java
@@ -1,7 +1,9 @@
 
 public class Bank {
 	public static void main(String[] args) {
-		
+		System.out.println("line sample 1");
+		System.out.println("line sample 2");
+		System.out.println("line sample 3");
 	}
 
 }
